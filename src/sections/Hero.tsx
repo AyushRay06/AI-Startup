236ca3c587f1ee8ae3cd7f1258db979cc559834a
AyushRay06 <rayayush47@gmail.com>
2024-10-09 04:20:48 +0530

    responsive

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -15,16 +15,16 @@ export const Hero = () => {
       {/* shadow */}
       <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,.5)_15%,rgb(14,0,36,.5)_78%,transparent)]"></div>
       {/* THE BIG PURPLE PLANET */}
-      <div className="absolute h-64 w-64 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
+      <div className="absolute h-64 w-64 lg:h-[500px] lg:w-[500px] bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
       {/* FIRST RING */}
-      <div className="absolute h-[344px] w-[344px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+      <div className="absolute h-[344px] w-[344px] lg:h-[640px] lg:w-[640px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
         {/* PLANET on the ring left-0 top-1/2 */}
-        <div className="absolute h-3 w-3 rounded-full bg-zinc-600 top-1/2 left-full -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="absolute h-3 w-3 lg:h-6 lg:w-6 rounded-full bg-zinc-600 top-1/2 left-full -translate-x-1/2 -translate-y-1/2"></div>
 
-        <div className="absolute h-3 w-3 rounded-full bg-zinc-600 top-1/2 left-0 -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="absolute h-6 w-6 rounded-full bg-zinc-600 top-1/2 left-0 -translate-x-1/2 -translate-y-1/2"></div>
       </div>
       {/* 2nd Ring */}
-      <div className="absolute h-[484px] w-[484px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute h-[484px] w-[484px] lg:hidden rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         {/* PLANET on 2nd Ring */}
         <div className="absolute h-4 w-4 bg-slate-800  rounded-full top-1/2 left-full -translate-x-1/2  -translate-y-1/2">
           {/* RING on Planet which is on the 2nd ring of the Main Planet */}
@@ -34,17 +34,17 @@ export const Hero = () => {
         <div className="absolute h-4 w-4 bg-slate-800 border border-white/20 rounded-full top-0 left-1/2 -translate-x-1/2  -translate-y-1/2"></div>
       </div>
       {/* 3rd Ring */}
-      <div className="absolute h-[699px] w-[699px] border rounded-full border-white/20 border-dashed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+      <div className="absolute h-[699px] w-[699px]   lg:h-[800px] lg:w-[800px] border rounded-full border-white/20 border-dashed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
         {/* PLANET on ring 3rd  ... cordinatest ir top an dleft through trial an error */}
-        <div className="absolute h-6 w-6 bg-slate-700 rounded-full top-1/4 left-7">
+        <div className="absolute h-4 w-4 lg:h-8 lg:w-8 bg-slate-700 rounded-full top-1/4 left-7">
           {/* RING on the planet which is on the 3rd ring of the main planet */}
           <div className="absolute h-[159px] w-[159px] border rounded-full border-white/20  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 "></div>
         </div>
       </div>
       {/* 4th Ring */}
-      <div className="absolute h-[899px] w-[899px] border rounded-full border-white/20 border-dashed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+      <div className="absolute h-[899px] w-[899px] lg:h-[1000px] lg:w-[1000px] border rounded-full border-white/20 border-dashed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
         {/* PLANET on the 4th ring */}
-        <div className="absolute h-8 w-8  bg-slate-800 rounded-full top-32 left-[764px]">
+        <div className="absolute h-6 w-6 lg:h-8 lg:w-8  bg-slate-800 rounded-full top-32 left-[764px] lg:left-[830px]">
           {/* RING on planet whixh is on the 4th ring of the main planet */}
           <div className="absolute h-[190px] w-[190px] border rounded-full border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
         </div>
@@ -52,10 +52,10 @@ export const Hero = () => {
 
       {/* CONTENT */}
       <div className="container relative mt-16">
-        <h1 className="text-center text-8xl md:text-9xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138))] text-transparent bg-clip-text">
+        <h1 className="text-center text-8xl md:text-9xl lg:text-[200px] font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138))] text-transparent bg-clip-text">
           AI SEO
         </h1>
-        <p className="text-center mt-6 text-2xl text-white/60">
+        <p className="text-center mt-6 text-2xl text-white/60 max-w-3xl mx-auto">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
           tenetur veniam eaque et nihil quia praesentium quasi rem facere? Optio
           non laboriosam maiores ducimus voluptas quisquam molestias voluptatum
